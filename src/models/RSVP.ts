d6d7be6f80a1a574840cbe1dcaec1a4c2fcae14c
Yeison Pencue <214280544+JeisonInnethic@users.noreply.github.com>
2025-08-23 19:35:00 -0500

    se agrega const de acompañante

--- src/models/RSVP.ts
+++ src/models/RSVP.ts
@@ -4,12 +4,15 @@ export interface IRSVP extends Document {
   name: string;
   phone: string;
   attending: boolean;
+  guests?: number;
+  
 }
 
 const rsvpSchema = new Schema<IRSVP>({
   name: { type: String, required: true },
   phone: { type: String, required: true },
   attending: { type: Boolean, required: true },
+  guests: { type: Number, default: 0 },
 });
 
 export const RSVPModel = model<IRSVP>("RSVP", rsvpSchema);
