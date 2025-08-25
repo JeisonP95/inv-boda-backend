15a000c49e6fd24af58141db322ee8954276ace7
Yeison Pencue <168775486+JeisonP95@users.noreply.github.com>
2025-08-25 09:11:03 -0500

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors({
+/*app.use(cors({
   origin: ["https://inv-boda.vercel.app", "http://localhost:3000"],
   credentials: true
-}));
+})); */
+app.use(cors());
 app.use(express.json());
+
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI!)
